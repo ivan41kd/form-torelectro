ed42018b2dffcad740b50fd33bb1e02fc4a81a9d
ivan41kd <grkorolevskiy@gmail.com>
2024-11-10 15:19:48 +0300

    fixed:slider

--- js/slider/slider.js
+++ js/slider/slider.js
@@ -15,34 +15,8 @@ function initSwiper() {
   scrollbar: {
    el: '.swiper-scrollbar',
   },
-  breakpoints: {
-   900: {
-    slidesPerView: 3,
-   },
-  },
  });
  swiper.slideTo(100, 0);
 }
-const swiper = new Swiper('.swiper', {
- // Optional parameters
- direction: 'horizontal',
- loop: true,
- slidesPerView: 'auto',
- spaceBetween: 20,
- lazyPreloadPrevNext: false,
 
- navigation: {
-  nextEl: '.catalog__next-icon',
- },
-
- // And if we need scrollbar
- scrollbar: {
-  el: '.swiper-scrollbar',
- },
- breakpoints: {
-  900: {
-   slidesPerView: 3,
-  },
- },
-});
 initSwiper();
